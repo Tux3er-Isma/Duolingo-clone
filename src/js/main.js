8122c937b49cf21057f94383041eddb91c305c64
Tux3er <isma.tux3er@gmail.com>
2023-02-12 21:48:25 +0100

    Adding Article Variables and Arrays and creating article function

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,14 @@ const main = document.querySelector('main');
 const article = document.querySelectorAll('.main__article');
 const articleTwo = document.querySelector('.main__article--two');
 const articleTwoTextContent = document.querySelector('.main__article--two__text-content');
-const imgRoutes = ['https://d35aaqx5ub95lt.cloudfront.net/images/1994201dce8a55a0017d59b58a035fc3.svg', 'https://d35aaqx5ub95lt.cloudfront.net/images/8f1e99a540ab7afcf5915e8852024e4c.svg', 'https://d35aaqx5ub95lt.cloudfront.net/images/splash/ebdcc22deaf0f85388fcb09e7ecfbe78.svg', 'https://d35aaqx5ub95lt.cloudfront.net/images/splash/a4d5fab3d1926c240cb2ae696b07d156.svg'];
+const imgRoutesArticleTwo = ['https://d35aaqx5ub95lt.cloudfront.net/images/1994201dce8a55a0017d59b58a035fc3.svg', 'https://d35aaqx5ub95lt.cloudfront.net/images/8f1e99a540ab7afcf5915e8852024e4c.svg', 'https://d35aaqx5ub95lt.cloudfront.net/images/splash/ebdcc22deaf0f85388fcb09e7ecfbe78.svg', 'https://d35aaqx5ub95lt.cloudfront.net/images/splash/a4d5fab3d1926c240cb2ae696b07d156.svg'];
+const imgRoutes = [];
+const articleName = ['three', 'five', 'seven'];
+const articleThree = document.querySelector('.main__article--three');
+const articleFour = document.querySelector('.main__article--four');
+const articleFive = document.querySelector('.main__article--five');
+const articleSix = document.querySelector('.main__article--six');
+const articleSeven = document.querySelector('.main__article--seven');
 
 //Conditions
 if (window.screen.availWidth <= 480){
@@ -66,10 +73,10 @@ if (window.screen.availWidth <= 480){
         }
 
         //Calling Functions
-        createContainer(imgRoutes[0], 'fire', 'Efectivo y eficiente', 'Nuestros cursos enseñan a leer, escuchar y hablar en otro idioma de forma efectiva y eficiente. ¿Ya viste nuestros estudios más recientes?');
-        createContainer(imgRoutes[1], 'correct', 'Aprendizaje personalizado', 'Al combinar lo mejor de la inteligencia artificial y las ciencias de idiomas, las lecciones se adaptan a tu desempeño para ayudarte a aprender al nivel adecuado y a tu propio ritmo.');
-        createContainer(imgRoutes[2], 'crown', 'Mantente motivado', 'Al sentirse como un juego y ofrecerte desafíos divertidos, hacemos que sea fácil formar un hábito de aprendizaje de idiomas. Además, siempre contarás con el apoyo y los recordatorios de nuestra amistosa mascota, Duo, el búho.');
-        createContainer(imgRoutes[3], 'woman', '¡Diviértete!', '¡El aprendizaje puede ser efectivo y divertido a la vez! Desarrolla tus habilidades cada día con ejercicios entretenidos y personajes entrañables.');
+        createContainer(imgRoutesArticleTwo[0], 'fire', 'Efectivo y eficiente', 'Nuestros cursos enseñan a leer, escuchar y hablar en otro idioma de forma efectiva y eficiente. ¿Ya viste nuestros estudios más recientes?');
+        createContainer(imgRoutesArticleTwo[1], 'correct', 'Aprendizaje personalizado', 'Al combinar lo mejor de la inteligencia artificial y las ciencias de idiomas, las lecciones se adaptan a tu desempeño para ayudarte a aprender al nivel adecuado y a tu propio ritmo.');
+        createContainer(imgRoutesArticleTwo[2], 'crown', 'Mantente motivado', 'Al sentirse como un juego y ofrecerte desafíos divertidos, hacemos que sea fácil formar un hábito de aprendizaje de idiomas. Además, siempre contarás con el apoyo y los recordatorios de nuestra amistosa mascota, Duo, el búho.');
+        createContainer(imgRoutesArticleTwo[3], 'woman', '¡Diviértete!', '¡El aprendizaje puede ser efectivo y divertido a la vez! Desarrolla tus habilidades cada día con ejercicios entretenidos y personajes entrañables.');
     }
 
     createMobileArticleTwo();
@@ -139,11 +146,44 @@ if (window.screen.availWidth <= 480){
         }
 
             //Calling Functions
-            createInformationContainers('first-block', imgRoutes[0], 'fire', 'Efectivo y eficiente', 'Nuestros cursos enseñan a leer, escuchar y hablar en otro idioma de forma efectiva y eficiente. ¿Ya viste nuestros estudios más recientes?');
-            createInformationContainers('first-block', imgRoutes[1], 'correct', 'Aprendizaje personalizado', 'Al combinar lo mejor de la inteligencia artificial y las ciencias de idiomas, las lecciones se adaptan a tu desempeño para ayudarte a aprender al nivel adecuado y a tu propio ritmo.');
-            createInformationContainers('second-block', imgRoutes[2], 'crown', 'Mantente motivado', 'Al sentirse como un juego y ofrecerte desafíos divertidos, hacemos que sea fácil formar un hábito de aprendizaje de idiomas. Además, siempre contarás con el apoyo y los recordatorios de nuestra amistosa mascota, Duo, el búho.');
-            createInformationContainers('second-block', imgRoutes[3], 'woman', '¡Diviértete!', '¡El aprendizaje puede ser efectivo y divertido a la vez! Desarrolla tus habilidades cada día con ejercicios entretenidos y personajes entrañables.');
+            createInformationContainers('first-block', imgRoutesArticleTwo[0], 'fire', 'Efectivo y eficiente', 'Nuestros cursos enseñan a leer, escuchar y hablar en otro idioma de forma efectiva y eficiente. ¿Ya viste nuestros estudios más recientes?');
+            createInformationContainers('first-block', imgRoutesArticleTwo[1], 'correct', 'Aprendizaje personalizado', 'Al combinar lo mejor de la inteligencia artificial y las ciencias de idiomas, las lecciones se adaptan a tu desempeño para ayudarte a aprender al nivel adecuado y a tu propio ritmo.');
+            createInformationContainers('second-block', imgRoutesArticleTwo[2], 'crown', 'Mantente motivado', 'Al sentirse como un juego y ofrecerte desafíos divertidos, hacemos que sea fácil formar un hábito de aprendizaje de idiomas. Además, siempre contarás con el apoyo y los recordatorios de nuestra amistosa mascota, Duo, el búho.');
+            createInformationContainers('second-block', imgRoutesArticleTwo[3], 'woman', '¡Diviértete!', '¡El aprendizaje puede ser efectivo y divertido a la vez! Desarrolla tus habilidades cada día con ejercicios entretenidos y personajes entrañables.');
     }
 
     createDesktopArticleTwo();
 }
+
+const createArticleStructure = (articleContainerDOM, imgRoute, imgAlt, titleContent, textCnt, linkContent) =>{
+    //Variables
+    const articleContainer = document.querySelector(`main__article--${articleContainerDOM}`);
+    let img = document.createElement('IMG');
+    const informationContainer = document.createElement('DIV');
+    let title = document.createElement('H1');
+    let text = document.createElement('P');
+    let link = document.createElement('A');
+
+    //Attributes and Content
+    img.setAttribute('src', imgRoute);
+    img.setAttribute(imgAlt);
+    title.textContent = titleContent;
+    text.textContent = textCnt;
+    link.textContent = linkContent;
+
+    //Classes
+    img.classList.add('main__article__img');
+    informationContainer.classList.add('main__article__information-container');
+    title.classList.addd('main__article__information-container__title');
+    text.classList.add('main__article__information-container__text');
+    link.classList.add('main__article__information-container__link');
+
+    //AppendChild
+    articleContainer.appendChild(img);
+    articleContainer.appendChild(informationContainer);
+    informationContainer.appendChild(title);
+    informationContainer.appendChild(text);
+    informationContainer.appendChild(link);
+}
+
+//Calling Functions
